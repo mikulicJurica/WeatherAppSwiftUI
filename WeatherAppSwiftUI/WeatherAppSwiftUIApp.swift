c6e3b51780104d7beae300e8684fd5ea1037db56
mikulicJurica <jurica.mikulic18@gmail.com>
2022-06-19 12:22:10 +0200

    Initial Commit

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Jurica Mikulic on 19.06.2022..
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
